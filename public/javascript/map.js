d71a06b8896971a5948aa644015d3a40aae2b1f8
Abhi Maradiya <abhimaradiya63@gmail.com>
2026-06-18 16:21:51 +0530

    added: Marker To Map

--- public/javascript/map.js
+++ public/javascript/map.js
@@ -3,6 +3,10 @@ let token=mapToken;
     
     const map = new mapboxgl.Map({
         container: 'map', // container ID
-        center: [-71.06776, 42.35816], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+        center: coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
         zoom: 9 // starting zoom
     });
+
+    const marker=new mapboxgl.Marker({color:'red'})
+    .setLngLat(coordinates)
+    .addTo(map);
